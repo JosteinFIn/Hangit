a9eb34e36b60a2a73640cf9e2f53963eaa5c865e
Jostein Finnekaasa <josteinfin@gmail.com>
2020-06-10 16:57:31 +0200

    Added masked word

--- Hangit.App/Program.cs
+++ Hangit.App/Program.cs
@@ -11,17 +11,26 @@ namespace Hangit.App
 		{
 			int guessesLeft = 10;
 			string guesses = "";
+			string secretWord = "GODFATHER";
+			string maskedWord = new String('-', secretWord.Length);
+
 			while(true)
 			{
-				string secretWord = "GODFATHER";
-
+				Console.Clear();
+				Console.WriteLine("\n"+MaskedSecretWord(secretWord, guesses));
 				Console.WriteLine("\n"+guesses);
 				Console.WriteLine("Guesses left: "+ guessesLeft);
-
 				Console.Write("Your guess: ");
 				string input = Console.ReadLine();
 
-				if(guessesLeft == 1)
+				if (guesses.Contains(input.ToUpper()))
+				{
+					Console.WriteLine("\nYou have allready guessed " + input.ToUpper());
+					continue;
+				}
+
+
+				if (guessesLeft == 1)
 				{
 					Console.WriteLine("Game Over");
 					break;
@@ -34,17 +43,15 @@ namespace Hangit.App
 						Console.ForegroundColor = ConsoleColor.Green;
 						Console.WriteLine("Correct");
 						Console.ResetColor();
+						guesses += input.ToUpper() + "  ";
 
-						if(!guesses.Contains(input.ToUpper()))
-						{
-							guesses += input.ToUpper() + "  ";
-						}
 					}
 					else
 					{
 						Console.ForegroundColor = ConsoleColor.Red;
 						Console.WriteLine("Wrong");
 						Console.ResetColor();
+						//guesses += input.ToUpper() + "  ";
 						guessesLeft--;
 					}
 				}
@@ -53,28 +60,53 @@ namespace Hangit.App
 					Console.WriteLine("Invalid guess");
 					guessesLeft--;
 				}
-
-				//if (!guesses.Contains(input.ToUpper()))
-				//{
-					//guesses += input.ToUpper() + "  ";
-				//}
-
-				//Console.WriteLine("GAme OvEr!");
 			}
 		}
-		static bool IsValid(string input) => Regex.IsMatch(input, @"^[A-ZÆØÅ]$");
-		/*
-		static string ToColors(string input)
+
+		private static string MaskedSecretWord(string secretWord, string guesses)
 		{
+			string validate = "";
+			char[] guessesLetters = guesses.ToCharArray(); ;
+			char[] secretLetters = secretWord.ToCharArray();
+
+			foreach (var letter in secretLetters)
+			{
+
+				if (LetterExistInSecretword(letter, guessesLetters))
+					validate += letter;
+				else
+					validate += '-';
+			}
+			return validate;
+		}
+
+		private static bool LetterExistInSecretword(char letter, char[] guessesLetters)
+		{
+			foreach(var guessLetter in guessesLetters)
+			{
+				if (guessLetter == letter)
+					return true;
+			}
+			return false;
+		}
+
+		static bool IsValid(string input) => Regex.IsMatch(input, @"^[A-ZÆØÅ]$");
+
+		//TEST//
+		static void ToColors(string input)
+		{
+			//string s = "";
 			Random random = new Random();
-			random.Next(0, 16);
-			//char[] c = input.ToCharArray();
-			int p = Enum.Parse(ConsoleColor, "Red");
-			//foreach (var letter in c)
-			//{
-				//random.Next(0, 16);
-				//Console.
-			//}
-		}*/
+			char[] c = input.ToCharArray();
+			foreach (var letter in c)
+			{
+				int rand = random.Next(1, 16);
+				ConsoleColor clr = (ConsoleColor)rand;
+				Console.ForegroundColor = clr;
+				Console.Write(letter);
+				Console.ResetColor();
+			}
+			Console.WriteLine("\n");
+		}
 	}
 }
