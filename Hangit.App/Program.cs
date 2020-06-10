197a12b9e7ca1f4784e782d3dcbd25d5f2814d64
Jostein Finnekaasa <josteinfin@gmail.com>
2020-06-10 10:29:34 +0200

    Added hellows!

--- Hangit.App/Program.cs
+++ Hangit.App/Program.cs
@@ -7,6 +7,11 @@ namespace Hangit.App
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");
+
 		}
 	}
 }
